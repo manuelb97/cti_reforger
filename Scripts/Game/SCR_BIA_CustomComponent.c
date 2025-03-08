10474ea51d1aede43cd8f8bc816665a4a290f647
disciplined-seal <61043432+manuelb97@users.noreply.github.com>
2025-03-08 15:38:20 +0100

    Implemented first version of a function to analyze the map for points of interest

--- Scripts/Game/SCR_BIA_CustomComponent.c
+++ Scripts/Game/SCR_BIA_CustomComponent.c
@@ -7,14 +7,108 @@ class SCR_BIA_CustomComponentClass : SCR_BaseGameModeComponentClass
 
 class SCR_BIA_CustomComponent : SCR_BaseGameModeComponent
 {
-	override void EOnInit(IEntity owner)
-	{
-		Print("=== Custom Component Initialized ===");
-	}
-	
-	//------------------------------------------------------------------------------------------------
-	override void OnPostInit(IEntity owner)
-	{	
+	private ref array<IEntity> foundBuildings = {};
+	private ref array<IEntity> nearbyBuildings = {};
+	private bool addBuilding;
+	private bool addNearbyBuilding;
 
+	bool AddEntity(IEntity ent)
+	{
+		if (!ent)
+			return true;
+			
+		if (addBuilding) 
+		{
+			foundBuildings.Insert(ent);
+		}
+		if (addNearbyBuilding) 
+		{
+			nearbyBuildings.Insert(ent);
+		}
+		return true;
 	}
+
+    void MarkHostileSectors()
+    {
+        Print("=== Marking Hostile Sectors ===");
+        
+        float searchRadius = 5000.0;
+		int minNearbyBuildings = 3;
+        float maxNearbyBuildingDist = 300.0;
+        float minDistBetweenSectors = 500.0;
+		array<vector> sectorCenters = {};
+
+        // Query entities by sphere to find buildings
+        vector mapCenter = Vector(0, 0, 0);
+		addBuilding = true;
+        GetGame().GetWorld().QueryEntitiesBySphere(mapCenter, searchRadius, 
+            AddEntity,
+            FilterBuildingEntities, 
+            EQueryEntitiesFlags.STATIC);
+		addBuilding = false;
+        
+        foreach (IEntity building : foundBuildings)
+        {
+            vector pos = building.GetOrigin();
+            nearbyBuildings.Clear();
+            
+			addNearbyBuilding = true;
+			GetGame().GetWorld().QueryEntitiesBySphere(pos, maxNearbyBuildingDist, 
+                AddEntity,
+                FilterBuildingEntities, 
+                EQueryEntitiesFlags.STATIC);
+			addNearbyBuilding = false;
+            
+            if (nearbyBuildings.Count() >= minNearbyBuildings)
+            {
+                bool tooClose = false;
+                foreach (vector sectorPos : sectorCenters)
+                {
+                    if (vector.Distance(sectorPos, pos) < minDistBetweenSectors)
+                    {
+                        tooClose = true;
+                        break;
+                    }
+                }
+                
+                if (!tooClose)
+                {
+                    sectorCenters.Insert(pos);
+                }
+            }
+        }
+        
+		if (sectorCenters.GetSizeOf() > 0)
+		{
+        	CreateMapMarkers(sectorCenters);
+		} else 
+		{
+			Print("no buildings found");
+		}
+    }
+
+    static bool FilterBuildingEntities(IEntity entity)
+    {
+        if (!entity) return false;
+
+        Resource resource;
+        string resourceName = resource.ToString();
+        return resourceName.Contains("barracks") || resourceName.Contains("cargo");
+    }
+
+    void CreateMapMarkers(array<vector> positions)
+    {
+        foreach (vector pos : positions)
+        {
+            SCR_MapMarkerBase marker = new SCR_MapMarkerBase();
+            marker.SetWorldPos(pos[0], pos[1]);
+            marker.SetColorEntry(Color.RED);
+        }
+    }
+	
+    override void OnPostInit(IEntity owner)
+    {
+        super.OnPostInit(owner);
+        MarkHostileSectors();
+    }
 }
